29828fc224af5809bad8a37a7509f18868f8e066
T-3B <cle.smagghe@gmail.com>
2024-04-23 13:36:33 +0200

    added option to extract addins

--- embed_addins.sh
+++ embed_addins.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find addins -mindepth 1 -type d -printf %P\\0 | parallel -0 '{ sed "/^#EOF#$/q" rhefo; find addins/x86-64/ -mindepth 1 -printf %P | tar c -b1 -C./addins/x86-64/ -T- | base64 -w600;} >rhefo_{}'
+find addins -mindepth 1 -type d -printf %P\\0 | parallel -0 '{ sed "/^#EOF#$/q" rhefo; find addins/{} -mindepth 1 -printf %P\\n | tar c -b1 -C./addins/{}/ -T- | base64 -w600;} >rhefo_{}; chmod +x rhefo_{}'
