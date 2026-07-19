d78b5f425d20e74d9970063d2c84e3aeb64cc84e
T-3B <cle.smagghe@gmail.com>
2026-07-19 23:39:41 +0200

    fix embed sh helper

--- embed_addins.sh
+++ embed_addins.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-find addins -mindepth 1 -type d -printf %P\\0 | parallel -0 '{ sed "/^#EOF#$/q" rhefo; find addins/{} -mindepth 1 -printf %P\\n | tar c -b1 -C./addins/{}/ -T- | base64 -w600;} >rhefo_{}; chmod +x rhefo_{}'
+archs=(x86-64 aarch64)
+
+for i in ${archs[@]}
+do	{
+		sed /^#EOF#$/q rhefo
+		eval find addins -mindepth 1 ! -name README.md $(printf "! -name '*_%s' " ${archs[@]/$i}) -printf '%P\\n' | tar c -b1 -C./addins/ --xform=s/_$i$// -T- | base64 -w600
+	} >rhefo_$i; chmod +x rhefo_$i
+done
